6723a8b1b707306b4a01a2f9b3bdd57f2304d14d
Andy <andy@britishideas.com>
2013-08-24 14:26:05 -0700

    Tweaks to help stop interferring with DVDFab Passkey scanning.

--- Software/PC/JacktheRipperBot/Drive.cs
+++ Software/PC/JacktheRipperBot/Drive.cs
@@ -17,7 +17,7 @@ namespace JacktheRipperBot
         /// <summary>
         /// Time to wait for disc to be scanned and made available in milliseconds
         /// </summary>
-        public long MaxDiscWaitTime = 120000L;
+        public long MaxDiscWaitTime = 180000L;
 
         [DllImport("winmm.dll")]
         static extern Int32 mciSendString(String command, StringBuilder buffer, Int32 bufferSize, IntPtr hwndCallback);
@@ -45,8 +45,9 @@ namespace JacktheRipperBot
             
             int Result1 = mciSendString(String.Format("open {0}: type CDAudio alias drive{0} shareable", DriveLetter), null, 0, IntPtr.Zero);
             int Result2 = mciSendString(String.Format("set drive{0} door open", DriveLetter), null, 0, IntPtr.Zero);
+            int Result3 = mciSendString(String.Format("close drive{0}", DriveLetter), null, 0, IntPtr.Zero);
 
-            Program.Log.OutputTimestampLine(String.Format("MCI Result codes: {0}, {1}", Result1, Result2));
+            Program.Log.OutputTimestampLine(String.Format("MCI Result codes: {0}, {1}, {2}", Result1, Result2, Result3));
         }
 
         /// <summary>
@@ -67,8 +68,11 @@ namespace JacktheRipperBot
                 DriveLetter = DriveLetter.TrimEnd(new char[] { ':', '\\' });
             }
 
-            mciSendString(String.Format("open {0}: type CDAudio alias drive{0}", DriveLetter), null, 0, IntPtr.Zero);
-            mciSendString(String.Format("set drive{0} door closed", DriveLetter), null, 0, IntPtr.Zero);
+            int Result1 = mciSendString(String.Format("open {0}: type CDAudio alias drive{0}", DriveLetter), null, 0, IntPtr.Zero);
+            int Result2 = mciSendString(String.Format("set drive{0} door closed", DriveLetter), null, 0, IntPtr.Zero);
+            int Result3 = mciSendString(String.Format("close drive{0}", DriveLetter), null, 0, IntPtr.Zero);
+
+            Program.Log.OutputTimestampLine(String.Format("MCI Result codes: {0}, {1}, {2}", Result1, Result2, Result3));
 
             if (Wait)
                 return WaitForDisc(Drive);
